633e98ac4f81992b687a999f556687265ff40c2d
john@work <johninquest@gmail.com>
2019-07-04 15:25:48 +0200

    added pouchdb to project

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -4,6 +4,7 @@ import { MomentDateAdapter } from '@angular/material-moment-adapter';
 import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 import { COUNTRYCODES } from '../countrycodes';
 import { from } from 'rxjs';
+// import PouchDB from 'pouchdb';
  
 /* See the Moment.js docs for the meaning of these formats:
  https://momentjs.com/docs/#/displaying/format */
