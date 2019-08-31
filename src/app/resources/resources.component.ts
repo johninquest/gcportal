458b57ac003f1657da9a7dac64ff322c42d4558b
john@home <johninquest@gmail.com>
2019-08-31 11:28:28 +0200

    resources content table now working

--- src/app/resources/resources.component.ts
+++ src/app/resources/resources.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  name: string;
+  // position: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {name: 'European Banking Authority (EBA)'},
+  {name: 'European Union Directives'},
+  {name: 'German Association of the Insured (BdV)'},
+  {name: 'German Insurance Association (GDV)'}
+];
+
 @Component({
   selector: 'app-resources',
   templateUrl: './resources.component.html',
   styleUrls: ['./resources.component.css']
 })
 export class ResourcesComponent implements OnInit {
+
+  displayedColumns: string[] = ['name'];
+  dataSource = ELEMENT_DATA;
 
   constructor() { }
 
