7b907f133937c74121748c57558a2535a918a9ad
john@home <johninquest@gmail.com>
2019-11-10 02:47:20 +0100

    improved css of displayed result

--- src/app/priceguide/priceguide.component.ts
+++ src/app/priceguide/priceguide.component.ts
@@ -29,11 +29,37 @@ export class PriceguideComponent implements OnInit {
     let tStart: string = this.startLoc.value;
     let tEnd: string = this.endLoc.value;
 
-    if (tStart === 'buea' && tEnd === 'douala') {
-      let tPrice: number = 1000;
-      let tDistance: number = 71.2;
-      return [tPrice, tDistance];
-    } else { 
+    if (tStart === 'muyuka')
+      if(tEnd === 'bamenda') {
+        let tPrice: string = '5500 XAF';
+        let tDistance: string = '300 KM';
+        return [tPrice, tDistance]; }
+      if(tEnd === 'buea') {
+        let tPrice: string = '500 XAF';
+        let tDistance: string = '26.2 KM';
+        return [tPrice, tDistance]; } 
+      if(tEnd === 'douala') {
+        let tPrice: string = '1200 XAF';
+        let tDistance: string = '90.1 KM';
+        return [tPrice, tDistance]; } 
+      if(tEnd === 'kumba') {
+        let tPrice: string = '1000 XAF';
+        let tDistance: string = '45.5 KM';
+        return [tPrice, tDistance]; }
+      if(tEnd === 'limbe') {
+        let tPrice: string = '800 XAF';
+        let tDistance: string = '49 KM';
+        return [tPrice, tDistance]; }
+      if(tEnd === 'mamfe') {
+          let tPrice: string = '3600 XAF';
+          let tDistance: string = '198 KM';
+          return [tPrice, tDistance];         }  
+      if(tEnd === 'tiko') {
+        let tPrice: string = '700 XAF';
+        let tDistance: string = '36.7 KM';
+        return [tPrice, tDistance]; } 
+      // else { ['N/A', 'N/A'] }     
+    else { 
       return ['N/A', 'N/A']; 
     }
   }
