4e38f9317282de93544419673c0a4b5cf9c6513e
john@work <johninquest@gmail.com>
2020-01-17 19:07:38 +0100

    started talking to database

--- src/app/records/records.component.ts
+++ src/app/records/records.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { DbService } from '../services/db.service';
+import moment from 'moment';
 
 @Component({
   selector: 'app-records',
@@ -8,8 +11,62 @@ import { Component, OnInit } from '@angular/core';
 
 export class RecordsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dbs: DbService) { }
 
-  ngOnInit() { }
+  toggleInput: boolean = false;
+  start = new FormControl(''); end = new FormControl('');
+  firstname = new FormControl(''); lastname = new FormControl('', Validators.required); 
+  nidn = new FormControl('') // National Id number
+  created = moment().format('YYYY-MM-DD HH:mm:ss');
+  tableData: any;
+
+  showAllData() { 
+    // return alert('Under construction');
+    let reqEndpoint: string = 'get_all_rows_in_table';
+    let targetTable: string = 'ticketsales';
+    let sqlPayload: object = { tb_name: targetTable };
+    let obs = this.dbs.getAllRowsInTable(reqEndpoint, sqlPayload);
+    obs.subscribe(
+      res => {
+        console.log(res); 
+        this.tableData = res; 
+      },
+      err => { console.log(err) }
+    );
+   }
+
+  addData() { 
+    let createData: object = {};
+    return alert('Under construction');
+   }
+
+  updateData() { 
+    let updateData: object = {};
+    return alert('Under construction');
+   }
+
+  deleteData() { 
+    return alert('Under construction');
+   }
+
+   showInput() { 
+     this.toggleInput = true; 
+    }
+
+   hideInput() { this.toggleInput = false }
+
+   dateFormater(dbDate: string) {
+     // let inDate = moment(dbDate).format('YYYY-MM-DD HH:mm:ss');
+     let outDate = moment(dbDate).format('DD.MM.YYYY HH:mm');
+     console.log(outDate);
+     return outDate;
+   }
+
+   displayedColumns: string[] = ['route', 'person', 'details'];
+
+  ngOnInit() {
+    // console.log(this.created);
+    this.showAllData();
+   }
 
 }
---
+++ src/app/services/db.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+
+export class DbService {
+
+  constructor(private http: HttpClient) { }
+
+  private dbUrl: string = 'http://localhost:3000/';
+
+  getAllRowsInTable(dbEndpoint: string, reqPayload: object): Observable<object> {
+    let dbUrl: string = this.dbUrl + dbEndpoint;
+    return this.http.post(dbUrl, reqPayload);
+  }
+}
