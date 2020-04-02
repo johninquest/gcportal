92428afc912af8e9f2110be01cc98723f1ecd7f9
john@work <johninquest@gmail.com>
2020-04-02 19:14:57 +0200

    started building a pothole tracker

--- src/app/potholetracker/potholetracker.component.ts
+++ src/app/potholetracker/potholetracker.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DbService } from '../services/db.service';
+import moment from 'moment';
 
 @Component({
   selector: 'app-potholetracker',
@@ -7,7 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PotholetrackerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dbs: DbService) {}
+
+  // GOOGLE CLOUD API KEY = AIzaSyDASOEZLqXaQhpncdDDZxuneXoo7wBubTw
+
+  saveGeoLocation() {
+    if(navigator.geolocation) {
+      console.log('Geolocation is supported!');
+      navigator.geolocation.getCurrentPosition( (position) => {
+        let lat = position.coords.latitude;
+        let lng = position.coords.longitude;
+        console.log([lat, lng]);
+        let reqEndpoint: string = 'save_pothole_data';
+        let reqPayload: object = { tb_name: 'potholes', geo_lat: lat, geo_lng: lng };
+        let obs = this.dbs.postRequest(reqEndpoint, reqPayload);
+        obs.subscribe(
+          res => console.log(res),
+          err => console.log(err)
+        );
+      });
+    }else {
+      console.log('Geolocation is not supported!');
+    }
+  }
 
   msg() { 
     alert('Under construction 👷🏾');
