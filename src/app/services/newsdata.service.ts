e2beef17e2251e2e0b32401bcd832e7f164ae197
john@work <johninquest@gmail.com>
2020-04-13 20:04:57 +0200

    added facebook pixel to site

--- src/app/services/newsdata.service.ts
+++ src/app/services/newsdata.service.ts
@@ -6,10 +6,7 @@ import { Observable } from 'rxjs';
 
 export class NewsdataService {
 
-  // private baseUrl: string = 'https://gnews.io/api/v3/search?token=1b9fa329965960737566f042fb2d86ac&lang=en&q=';
   private baseUrl: string = 'https://newsapi.org/v2/everything?apiKey=bc80aee5a6654843bd745e416bccc24d&sortBy=publishedAt&q=' 
-  // Google Custom Search JSON API Key = AIzaSyAbUBuq98veydP52oAe-8LvrKxqTLpNbJ4 
-  // private baseUrl: string =  'https://www.googleapis.com/customsearch/v1?key=AIzaSyAbUBuq98veydP52oAe-8LvrKxqTLpNbJ4&cx=011742578395077431650:pemrlwdwwbp';
 
   constructor(private http: HttpClient) {}
 
