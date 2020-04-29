6a082bf84800dcdea108b8912f5ae5f0ab9f774e
john@work <johninquest@gmail.com>
2020-04-29 19:20:05 +0200

    added tax calculator function to ticket page

--- src/app/ticket/ticket.component.ts
+++ src/app/ticket/ticket.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { PLACES } from '../destinations';
+import moment from 'moment';
+moment.locale('de');
 
 export interface placesListDesc {
   value: string;
@@ -22,9 +24,39 @@ export class TicketComponent implements OnInit {
   ticketOwnerId = new FormControl(''); ticketNumber = new FormControl('');
   startPlaces: placesListDesc[] = PLACES;
   endPlaces: placesListDesc[] = PLACES;
+  toggleInput: boolean = true; togglePreview: boolean = false; toggleButtons: boolean = false;
+  tDate = moment().format('lll');
 
   previewTicket() {
-    alert('Under construction ... 🚧');
+    this.toggleInput = false;
+    this.togglePreview = true;
+    this.toggleButtons = true;
+    // alert('Under construction ... 🚧');
+  }
+
+  backToInput() {
+    this.toggleButtons = false;
+    this.togglePreview = false;
+    this.toggleInput = true;
+  }
+
+  calculateTaxTotal(feeBeforeTax: number) {
+    let taxRate: number = 19.25;
+    let taxTotal: number = (taxRate / 100) * feeBeforeTax;
+    let feeAfterTax: number = taxTotal + feeBeforeTax;
+    if(feeBeforeTax) {
+      return [Math.round(taxTotal), Math.round(feeAfterTax)]; 
+    }else {
+      return ['', ''];
+    }  
+  }
+
+  saveAsImage() {
+    alert('Under construction ...');
+  }
+
+  saveAsPdf() {
+    alert('Under construction ...');
   }
 
   ngOnInit(): void { }
