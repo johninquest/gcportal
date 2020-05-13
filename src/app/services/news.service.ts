f3759004dc6abf8d03dec5fd9a96e7744872040f
john@work <johninquest@gmail.com>
2020-05-13 18:35:42 +0200

    a little clean up

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -7,18 +7,11 @@ import { Observable } from 'rxjs';
 export class NewsService {
 
   private baseUrl: string = 'https://newsapi.org/v2/everything?apiKey=bc80aee5a6654843bd745e416bccc24d&sortBy=publishedAt&q=';
-  private googleNewsBaseUrl: string = 'https://news.google.com/news/rss/headlines/section/geo/'; 
-  private corsUrl: string = 'https://cors-anywhere.herokuapp.com/';
 
   constructor(private http: HttpClient) {}
 
   getNewsData(reqTarget: string): Observable<any> {
     let reqUrl: string = this.baseUrl + reqTarget;
-    return this.http.get(reqUrl);
-  }
-
-  getGoogleNews(reqTarget: string): Observable<any> {
-    let reqUrl: string = this.corsUrl + this.googleNewsBaseUrl + reqTarget;
     return this.http.get(reqUrl);
   }
 
