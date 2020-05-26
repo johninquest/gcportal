bb0260b890129ab3e09a42b60d4d01aa87ddea21
john@work <johninquest@gmail.com>
2020-05-26 15:49:54 +0200

    some more wording improvements

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,7 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-
 export class AboutComponent implements OnInit {
 
   constructor() { }
