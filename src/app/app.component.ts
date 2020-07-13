013c63237f9133830d55931c16548fef7d39e5dd
john@work <johninquest@gmail.com>
2020-07-13 15:47:16 +0200

    updated form ui experience

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,20 @@
-import { Component } from '@angular/core';
-import { filter } from 'rxjs/operators';
-import { Router, NavigationEnd } from '@angular/router';
-import { from } from 'rxjs';
+import { Component } from "@angular/core";
+import { filter } from "rxjs/operators";
+import { Router, NavigationEnd } from "@angular/router";
+import { from } from "rxjs";
 
 declare const gtag: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-
 export class AppComponent {
-  
-  constructor(router: Router) {
-    const navEndEvent$ = router.events.pipe(filter(event => event instanceof NavigationEnd));
+  constructor(router: Router) {}
+  /*     const navEndEvent$ = router.events.pipe(filter(event => event instanceof NavigationEnd));
     navEndEvent$.subscribe((event: NavigationEnd) => {
       gtag('config', 'UA-137248590-3', {'page_path': event.urlAfterRedirects});
     });
-  }
+  } */
 }
--- src/app/guest/guest.component.ts
+++ src/app/guest/guest.component.ts
@@ -1,40 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms'; 
-import moment from 'moment';
-// import 'moment/locale/de'; 
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import moment from "moment";
+// import 'moment/locale/de';
 // moment.locale('de');
 
 @Component({
-  selector: 'app-guest',
-  templateUrl: './guest.component.html',
-  styleUrls: ['./guest.component.css']
+  selector: "app-guest",
+  templateUrl: "./guest.component.html",
+  styleUrls: ["./guest.component.css"],
 })
 export class GuestComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   toggleInput: boolean = true;
   togglePreview: boolean = false;
   toggleButtons: boolean = false;
   datetimeStamp: string;
 
-  guestInfoForm = new FormGroup({
-    lastName: new FormControl('', Validators.required),
-    arrivedAt: new FormControl(''),
-    departedAt: new FormControl(''),
-    tableNumber: new FormControl(''),
-    phoneNumber: new FormControl(''),
-    eMail: new FormControl('')  
-  });
+  // Guest Data
+  guestNames = new FormControl("", [Validators.required]);
+  // guestAddress = new FormControl("");
+  guestPhoneNumber = new FormControl("");
+  guestEmail = new FormControl("", [Validators.email]);
+  guestArrivalDate = new FormControl("");
+  guestDepartureDate = new FormControl("");
+  tableNumber = new FormControl("");
+
+  // Business Data
+  businessName = new FormControl("", [Validators.required]);
+  businessAddress = new FormControl("");
+  businessPhoneNumber = new FormControl("");
+  businessEmail = new FormControl("", [Validators.email]);
 
   preview() {
     this.toggleInput = false;
     this.togglePreview = true;
     this.toggleButtons = true;
-    console.log(this.guestInfoForm.value);
+    // console.log(this.guestInfoForm.value);
 
     // this.datetimeStamp = moment().format('LLL');
-    this.datetimeStamp = moment().locale('de').format('LLL');
+    this.datetimeStamp = moment().locale("de").format("LLL");
   }
 
   backToInput() {
@@ -44,13 +49,13 @@ export class GuestComponent implements OnInit {
   }
 
   formatPrintDateTime(dt: string) {
-    if(dt) {
-      let outputDate: string = moment(dt).locale('de').format('L');
-      let outputTime: string = moment(dt).locale('de').format('LT');
-      let outputDateTime = outputDate + ' ' + outputTime;
+    if (dt) {
+      let outputDate: string = moment(dt).locale("de").format("L");
+      let outputTime: string = moment(dt).locale("de").format("LT");
+      let outputDateTime = outputDate + " " + outputTime;
       return outputDateTime;
-    }else {
-      return '';
+    } else {
+      return "";
     }
   }
 
@@ -61,9 +66,8 @@ export class GuestComponent implements OnInit {
   saveAsPdf() {}
 
   ucMessage() {
-    alert('Ist noch eine Baustelle');
+    alert("🚧 BAUSTELLE 🚧");
   }
 
-  ngOnInit(): void { }
-
+  ngOnInit(): void {}
 }
