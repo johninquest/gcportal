65cbb35c1d5e51e77b8aeb383b153de95ac95960
john@work <johninquest@gmail.com>
2020-08-19 13:55:21 +0200

    trying to fix issue with live function

--- src/app/invoice/invoice.component.ts
+++ src/app/invoice/invoice.component.ts
@@ -20,7 +20,6 @@ export class InvoiceComponent implements OnInit {
     invoiceNumber: new FormControl(""),
     amountBeforeTax: new FormControl(""),
     taxValue: new FormControl(""),
-    amountAfterTax: new FormControl({ value: "", disabled: true }),
     paidBy: new FormControl(""),
     paidFor: new FormControl(""),
     transactionLocation: new FormControl(""),
@@ -28,20 +27,39 @@ export class InvoiceComponent implements OnInit {
     furtherInfos: new FormControl(""),
   });
 
+  afterTaxValue: string = "";
+
   vats: Vatdesc[] = [
     { value: 0, viewValue: "0 %" },
     { value: 5, viewValue: "5 %" },
     { value: 16, viewValue: "16 %" },
   ];
 
-  logInputs() {
-    console.log(this.invoiceForm.value);
+  showAfterTaxTotal() {
+    let _amount: number = this.invoiceForm.get("amountBeforeTax").value;
+    let _tax: number = this.invoiceForm.get("taxValue").value;
+    if (_amount && _tax) {
+      return (this.afterTaxValue = this.cs.calculateVAT(_amount, _tax));
+    } else {
+      return "";
+    }
+    /*  console.log(this.cs.calculateVAT(_amount, _tax));
+    returnthis.afterTaxValue = this.cs.calculateVAT(_amount, _tax);
+    //return (this.afterTaxValue = this.cs.calculateVAT(_amount, _tax)); */
+  }
+
+  onChanges(): void {
+    this.invoiceForm.controls["amountBeforeTax"].valueChanges.subscribe(() => {
+      this.showAfterTaxTotal();
+    });
+    this.invoiceForm.controls["taxValue"].valueChanges.subscribe(() => {
+      this.showAfterTaxTotal();
+    });
   }
 
   ucMessage() {
-    alert("Noch eine Baustelle 🚧");
-    this.logInputs();
+    // alert("Noch eine Baustelle 🚧");
+    this.showAfterTaxTotal();
   }
-
   ngOnInit(): void {}
 }
--- src/app/services/calculator.service.ts
+++ src/app/services/calculator.service.ts
@@ -6,9 +6,16 @@ import { Injectable } from "@angular/core";
 export class CalculatorService {
   constructor() {}
 
-  calculateVAT(requestedVAT: number) {
-    let calculatedVAT: number = 10;
-    let calculatedTotal: number = 25;
-    return [calculatedVAT, calculatedTotal];
+  calculateVAT(amount: number, tax: number) {
+    if (amount && tax) {
+      const total_tax_paid = (tax / 100) * amount;
+      const total_plus_tax = +total_tax_paid + +amount;
+      // console.log(total_plus_tax);
+      return total_plus_tax.toFixed(2);
+    }
+    if (tax === 0) {
+      // console.log(amount);
+      return amount.toFixed(2);
+    }
   }
 }
