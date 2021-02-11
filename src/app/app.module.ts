67aba8949d006bee20796fd9b068c28e2dd4501c
john@work <johninquest@gmail.com>
2021-02-11 16:38:39 +0100

    implemented navigation component + improved solutions page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ContactComponent } from "./contact/contact.component";
 import { ServiceWorkerModule } from "@angular/service-worker";
 import { environment } from "../environments/environment";
 import { SolutionsComponent } from "./solutions/solutions.component";
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { SolutionsComponent } from "./solutions/solutions.component";
     AboutComponent,
     ContactComponent,
     SolutionsComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
