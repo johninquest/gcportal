758763bdd3dcc37ebe7dcbd6da315b488d290a8b
john@work <johninquest@gmail.com>
2021-11-02 09:57:33 +0100

    updated sitemap

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,12 +5,12 @@ import { Component, OnInit } from "@angular/core";
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.css"],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   constructor() {}
 
   comingSoonMessage() {
-    alert("DEMNÄCHST VERFÜGBAR");
+    alert("Demnächst verfügbar");
   }
 
-  ngOnInit() {}
+  // ngOnInit() {}
 }
