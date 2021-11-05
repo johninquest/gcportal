eb9ffd91f2c2da379f1873853b1bd45d44ee8e1b
john@work <johninquest@gmail.com>
2021-11-05 12:19:54 +0100

    removed dead code

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from "@angular/core";
-// import { FormControl } from "@angular/forms";
-// import { TranslateService } from "@ngx-translate/core";
+import { Component } from "@angular/core";
 
 interface Language {
   value: string;
@@ -12,36 +10,6 @@ interface Language {
   templateUrl: "./nav.component.html",
   styleUrls: ["./nav.component.css"],
 })
-export class NavComponent implements OnInit {
+export class NavComponent {
   constructor() {}
-
-  /*   constructor(private translate: TranslateService) {
-    // translate.addLangs(["en", "de"]);
-    translate.setDefaultLang(this.getBrowserLanguage(navigator.language));
-  }
-
-  getBrowserLanguage(sysLanguage: string) {
-    let languageToLowerCase = sysLanguage.toLocaleLowerCase();
-    let isDE = languageToLowerCase.startsWith("de");
-    if (isDE) {
-      return "de";
-    } else {
-      return "en";
-    }
-  }
-
-  selectedLanguage = new FormControl("en");
-
-  languages: Language[] = [
-    { value: "en", viewValue: "EN" },
-    { value: "de", viewValue: "DE" },
-  ];
-
-  useLanguage(myValue: string) {
-    console.log("Language has changed!");
-    console.log(this.selectedLanguage.value);
-    this.translate.use(this.selectedLanguage.value);
-  }
- */
-  ngOnInit(): void {}
 }
