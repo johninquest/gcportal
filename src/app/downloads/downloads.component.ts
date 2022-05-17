7843dcd4c66e1ac6bc3d128822ebb514402b99b9
john@work <johninquest@gmail.com>
2022-05-17 13:36:50 +0200

    can now download the ldgr apk via firebase storage

--- src/app/downloads/downloads.component.ts
+++ src/app/downloads/downloads.component.ts
@@ -9,6 +9,13 @@ export class DownloadsComponent {
   constructor() {}
 
   startDownload() {
-    alert("Coming soon ...");
+    let targetUrl: string =
+      "https://firebasestorage.googleapis.com/v0/b/johnappsde.appspot.com/o/ldgr-app-v15.apk?alt=media&token=6b3ba3f9-8b0a-4030-8b68-63ddcc891c1d";
+    window.open(targetUrl, "_self");
+    window.focus();
+  }
+
+  comingSoon() {
+    alert("Version coming soon");
   }
 }
