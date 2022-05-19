bdc2ec7e901666ece1353db5a400c75740aafa9a
john@work <johninquest@gmail.com>
2022-05-19 13:01:38 +0200

    added access to latest ldgr app version

--- src/app/downloads/downloads.component.ts
+++ src/app/downloads/downloads.component.ts
@@ -8,11 +8,17 @@ import { Component, OnInit } from "@angular/core";
 export class DownloadsComponent {
   constructor() {}
 
-  startDownload() {
+  startDownloadV15() {
     let targetUrl: string =
       "https://firebasestorage.googleapis.com/v0/b/johnappsde.appspot.com/o/ldgr-app-v15.apk?alt=media&token=6b3ba3f9-8b0a-4030-8b68-63ddcc891c1d";
     window.open(targetUrl, "_self");
     window.focus();
+  }
+
+  startDownloadV16() {
+    let targetUrl: string =
+      "https://firebasestorage.googleapis.com/v0/b/johnappsde.appspot.com/o/ldgr-app-v16.apk?alt=media&token=4085d26a-5b25-4712-908a-ed1f4de8e188";
+    window.open(targetUrl, "_self");
   }
 
   comingSoon() {
