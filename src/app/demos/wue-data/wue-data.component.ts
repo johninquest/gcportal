9e0d960ceca7d676c0e219cf4496099168aabb7f
john@work <johninquest@gmail.com>
2022-07-20 16:08:34 +0200

    can now add group records from same day

--- src/app/demos/wue-data/wue-data.component.ts
+++ src/app/demos/wue-data/wue-data.component.ts
@@ -36,8 +36,9 @@ export class WueDataComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.getSoilData();
+    // this.getSoilHumidityData();
     this.getWeatherData();
+    this.getSoilHumidityData2();
     // this.probeChart();
   }
 
@@ -47,24 +48,47 @@ export class WueDataComponent implements OnInit {
   rainFallDate: Array<any> = [];
   soilHumidityVals: Array<number> = [];
   soilHumidityCoordsX: Array<any> = [];
+  soilHumidityByDayYCoords: Array<number> = [];
+  soilHumidityByDayXCoords: Array<any> = [];
 
-  getSoilData() {
+  getSoilHumidityData() {
     let _url: string =
-      "https://opendata.wuerzburg.de/api/records/1.0/search/?dataset=baeren-bodenfeuchte&q=&rows=10000&refine.time=2022&sort=record_timestamp";
+      "https://opendata.wuerzburg.de/api/records/1.0/search/?dataset=baeren-bodenfeuchte&q=&rows=3000&refine.time=2022&sort=record_timestamp";
     let _req = this._ws.getRequest(_url);
     _req.subscribe(
       (res) => {
         this.soilHumidityData = res;
-        // console.log("Records =>", res["records"]);
         this.soilHumidityVals = res["records"].map(
           (data: any) => data["fields"]["volwatercontent_1"]
         );
         this.soilHumidityCoordsX = res["records"].map((data: any) =>
-          dayjs(data["fields"]["time"]).format("DD.MM.YYYY")
+          dayjs(data["record_timestamp"]).format("YYYY-MM-DD")
         );
-        /* (data: any) => data["fields"]["time"]
-        ); */
-        this.plotSoilHumidityByDay(res);
+        this.plotSoilHumidity(res);
+      },
+      (err) => console.log("Error =>", err)
+    );
+  }
+
+  getSoilHumidityData2() {
+    let _url: string =
+      "https://opendata.wuerzburg.de/api/records/1.0/search/?dataset=baeren-bodenfeuchte&q=&rows=3000&refine.time=2022&sort=record_timestamp";
+    let _newList: Array<object> = [];
+    let _req = this._ws.getRequest(_url);
+    _req.subscribe(
+      (res) => {
+        let _records: Array<object> = res["records"];
+        _records.map((data) => {
+          let _obj = {
+            record_timestamp: dayjs(data["record_timestamp"]).format(
+              "YYYY-MM-DD"
+            ),
+            volwatercontent_1: data["fields"]["volwatercontent_1"],
+          };
+          _newList.push(_obj);
+        });
+        // console.log("New list =>", _newList);
+        this.groupRecordsFromSameDay(_newList);
       },
       (err) => console.log("Error =>", err)
     );
@@ -72,7 +96,7 @@ export class WueDataComponent implements OnInit {
 
   getWeatherData() {
     let _url: string =
-      "https://opendata.wuerzburg.de/api/records/1.0/search/?dataset=wetter_wue_2022&q=&rows=5000&sort=dt_iso&facet=niederschlag";
+      "https://opendata.wuerzburg.de/api/records/1.0/search/?dataset=wetter_wue_2022&q=&rows=3000&sort=dt_iso&facet=niederschlag";
     let _req = this._ws.getRequest(_url);
     _req.subscribe(
       (res) => {
@@ -89,11 +113,37 @@ export class WueDataComponent implements OnInit {
     );
   }
 
+  groupRecordsFromSameDay(arr: Array<object>) {
+    let res: Array<object> = arr.reduce((acc: any, obj: any) => {
+      let existObj = acc.find(
+        (item: any) => item["record_timestamp"] === obj["record_timestamp"]
+      );
+      if (existObj) {
+        existObj["volwatercontent_1"] =
+          existObj["volwatercontent_1"] + obj["volwatercontent_1"];
+        return acc;
+      }
+      acc.push(obj);
+      return acc;
+    }, []);
+    // res.sort((a, b) => dayjs(a.record_timestamp) - dayjs(b.record_timestamp));
+    // res.reverse();
+    console.log(
+      "Grouped data after summing values from same date =>",
+      res.reverse()
+    );
+    this.soilHumidityByDayYCoords = res.map(
+      (data) => data["volwatercontent_1"]
+    );
+    this.soilHumidityByDayXCoords = res.map((data) => data["record_timestamp"]);
+    this.plotSoilHumidityByDay();
+  }
+
   probeChart(chartData: any) {
     // const ctx = document.getElementById("myChart").getContext("2d");
     let _xAxisData = this.rainFallDate.reverse();
     let _yAxisData = this.rainFallQty.reverse();
-    let _chart = new Chart("myChart", {
+    let _chart = new Chart("weatherRainfallChart", {
       type: "bar",
       data: {
         labels: _xAxisData,
@@ -149,7 +199,7 @@ export class WueDataComponent implements OnInit {
     return _chart;
   }
 
-  plotSoilHumidityByDay(rawData: any) {
+  plotSoilHumidity(rawData: any) {
     let _chart = new Chart("soilHumidityChart", {
       type: "bar",
       data: {
@@ -158,6 +208,59 @@ export class WueDataComponent implements OnInit {
           {
             label: "Bodenfeuchtigkeit - Hubland / Würzburg",
             data: this.soilHumidityVals.reverse(),
+            backgroundColor: [
+              "rgba(182, 31, 56, 255)",
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(182, 31, 56, 255)",
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+        plugins: {
+          legend: {
+            display: true,
+            labels: {
+              font: {
+                size: 15,
+                weight: "Bold",
+              },
+            },
+          },
+        },
+      },
+    });
+    return _chart;
+  }
+
+  plotSoilHumidityByDay() {
+    let _chart = new Chart("soilHumidityByDayChart", {
+      type: "bar",
+      data: {
+        labels: this.soilHumidityByDayXCoords,
+        datasets: [
+          {
+            label: "Bodenfeuchtigkeit - Hubland / Würzburg pro Tag",
+            data: this.soilHumidityByDayYCoords,
             backgroundColor: [
               "rgba(182, 31, 56, 255)",
               "rgba(255, 99, 132, 0.2)",
