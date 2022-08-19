5bd11359d14605a260df999d14edc2937e3a40fc
john@work <johninquest@gmail.com>
2022-08-19 14:02:21 +0200

    improved age calculator

--- src/app/age-calculator/age-calculator.component.ts
+++ src/app/age-calculator/age-calculator.component.ts
@@ -8,17 +8,35 @@ import { CalculatorService } from "../services/calculator.service";
   styleUrls: ["./age-calculator.component.scss"],
 })
 export class AgeCalculatorComponent {
-  constructor(private cs: CalculatorService) {}
+  constructor(private _calculator: CalculatorService) {}
   ageForm = new FormGroup({
     selectedDate: new FormControl(""),
   });
 
-  showAge(dateStr: any) {
-    let ageFromDate = this.cs.ageFromDate2(dateStr);
+  /*   showAge(dateStr: string) {
+    let ageFromDate = this._calculator.ageFromDate2(dateStr);
     if (ageFromDate) {
       return ageFromDate;
     } else {
       return "";
     }
+  } */
+
+  showAgeInYears(pickedDate: string) {
+    return this._calculator.ageFromDate(pickedDate)["inYears"];
+  }
+
+  showAgeInMonths(pickedDate: string) {
+    return this._calculator.ageFromDate(pickedDate)["inMonths"];
+  }
+
+  showAgeInDays(pickedDate: string) {
+    return this._calculator.ageFromDate(pickedDate)["inDays"];
+  }
+
+  showGeneration(pickedDate: string) {
+    let _generation: string =
+      this._calculator.generationFromDateOfBirth(pickedDate);
+    return _generation;
   }
 }
--- src/app/services/calculator.service.ts
+++ src/app/services/calculator.service.ts
@@ -21,13 +21,25 @@ export class CalculatorService {
   ageFromDate(providedDate: string) {
     if (providedDate) {
       let now = dayjs(new Date());
-      let diff = now.diff(providedDate, "year");
-      return diff;
+      let diffY: number = now.diff(providedDate, "year");
+      let diffM: number = now.diff(providedDate, "month");
+      let diffD: number = now.diff(providedDate, "day");
+      let _age: object = {
+        inYears: diffY,
+        inMonths: diffM,
+        inDays: diffD,
+      };
+      return _age;
     } else {
-      return "";
+      let _age: object = {
+        inYears: 0,
+        inMonths: 0,
+        inDays: 0,
+      };
+      return _age;
     }
   }
-
+  /* 
   ageFromDate2(providedDate: string) {
     if (providedDate) {
       let now = dayjs(new Date());
@@ -59,6 +71,28 @@ export class CalculatorService {
       let _months: string = "months";
       let _years: string = "years";
       return { day: _days, month: _months, year: _years };
+    }
+  }
+ */
+  generationFromDateOfBirth(providedDate: string) {
+    let _yearOfBirth: number = dayjs(providedDate).year() ?? 0;
+    if (_yearOfBirth >= 1928 && _yearOfBirth <= 1945) {
+      return "silent";
+    }
+    if (_yearOfBirth >= 1946 && _yearOfBirth <= 1964) {
+      return "boomer";
+    }
+    if (_yearOfBirth >= 1965 && _yearOfBirth <= 1980) {
+      return "x";
+    }
+    if (_yearOfBirth >= 1981 && _yearOfBirth <= 1996) {
+      return "y";
+    }
+    if (_yearOfBirth >= 1997 && _yearOfBirth <= 2010) {
+      return "z";
+    }
+    if (_yearOfBirth >= 2011 && _yearOfBirth <= 2025) {
+      return "alpha";
     }
   }
 }
